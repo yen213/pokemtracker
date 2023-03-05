6f8ee0939dd9845b7f8ab2693ebedc733c4d3b44
Yen <hmaruf29@gmail.com>
2023-03-05 04:24:35 -0800

    add call to load and render list from the db if logged in

--- client/src/HomePage.tsx
+++ client/src/HomePage.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
+
 import FloatingMenu from "./menu/FloatingMenu";
 
+import { getPokemonList } from "./axios/pokemon.api";
+import { pokemonData } from "./data/pokemon";
 import { AppHeader } from "./AppHeader";
 import { PokemonListContainer } from "./pokemonList/PokemonListContainer";
 
 // Renders the main page of the application
 const HomePage = () => {
+    const [pokemonList, setPokemonList] = useState(pokemonData); // DB list
+
+    // Load the list of Pokemon from the database
+    useEffect(() => {
+        const getList = async () => {
+            const res = await getPokemonList();
+            const list = res.data.list;
+
+            if (res.status === 200 && list != null && list.length > 0) {
+                console.log(list);
+                setPokemonList(list);
+            }
+        };
+
+        getList();
+    }, []);
+
     return (
         <div className="antialiased font-open-sans bg-gradient-to-b from-sky-100 to-sky-300 to-sky-400 min-h-screen">
             <AppHeader />
@@ -16,8 +37,8 @@ const HomePage = () => {
                 </p>
                 <p>All you have to do is click the Poke Ball icon to set a Pokemon as caught or uncaught.</p>
                 <p>Use the search box below to search by a Pokemon's name or dex number.</p>
-                <PokemonListContainer />
-                <FloatingMenu />
+                <PokemonListContainer pokemonList={pokemonList} setPokemonList={setPokemonList} />
+                <FloatingMenu setPokemonList={setPokemonList} />
             </div>
         </div>
     );
