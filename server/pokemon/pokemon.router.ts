966e17d65d1c6228772a1226e36f4238548e6910
Yen <hmaruf29@gmail.com>
2023-03-04 22:21:53 -0800

    add validator before the pokemon api call so that only authorized users can call endpoint

--- server/pokemon/pokemon.router.ts
+++ server/pokemon/pokemon.router.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
 import { createPokemon, deletePokemon, getPokemonList, updatePokemon } from "./pokemon.controller";
+import { isValidUser } from "../validate.user";
 import { pokemonValidator, pokemonValidatorResult } from "./pokemon.validation";
 
 export const pokemonRouter = express.Router();
 
 // Pokemon router endpoints
-pokemonRouter.get("/list", getPokemonList);
-pokemonRouter.post("/add", pokemonValidator, pokemonValidatorResult, createPokemon);
-pokemonRouter.put("/update", pokemonValidator, pokemonValidatorResult, updatePokemon);
-pokemonRouter.delete("/delete", deletePokemon);
+pokemonRouter.get("/list", isValidUser, getPokemonList);
+pokemonRouter.post("/add", isValidUser, pokemonValidator, pokemonValidatorResult, createPokemon);
+pokemonRouter.put("/update", isValidUser, pokemonValidator, pokemonValidatorResult, updatePokemon);
+pokemonRouter.delete("/delete", isValidUser, deletePokemon);
