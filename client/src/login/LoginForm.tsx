900a217298be388ba9e1fd9044e996f5eb417622
Yen <hmaruf29@gmail.com>
2023-03-04 23:21:16 -0800

    add functionality for logging in

--- client/src/login/LoginForm.tsx
+++ client/src/login/LoginForm.tsx
@@ -1,44 +1,85 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 import EyeIcon from "../icons/EyeIcon";
 import LockIcon from "../icons/LockIcon";
 import LoginIcon from "../icons/LoginIcon";
 
+// Renders the form for the login page
 const LoginForm = () => {
     const [viewPassword, setViewPassword] = useState(false);
-    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
     const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    // Logs in a user to the app and routes them to the home page
+    const login = (e: React.MouseEvent) => {
+        e.preventDefault();
+
+        // Make sure both fields has values
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            if (email.trim().length === 0) {
+                setIsEmailValid(false);
+            }
+
+            if (password.trim().length === 0) {
+                setIsPasswordValid(false);
+            }
+
+            return;
+        }
+
+        axios({
+            method: "post",
+            data: { username: email.trim(), password: password.trim() },
+            withCredentials: true,
+            url: "http://localhost:3001/auth/login",
+        }).then(
+            () => navigate("/"),
+            (err) => console.log(err.response)
+        );
+    };
 
     return (
         <div className="flex place-items-center antialiased font-open-sans bg-gradient-to-b from-sky-100 to-sky-300 to-sky-400 min-h-screen">
             <div className="flex flex-col mx-auto items-center w-full md:w-3/5 lg:w-4/6">
                 <form className="flex flex-col lg:w-1/2 w-8/12">
-                    <div className="flex flex-wrap items-stretch w-full mb-4 relative h-15 bg-white items-center rounded mb-6 pr-10">
+                    <div className="flex flex-wrap items-stretch w-full relative h-15 bg-white items-center rounded pr-10 mb-5">
                         <div className="flex -mr-px justify-center w-15 p-4">
-                            <LoginIcon iconColor="text-neutral-600" />
+                            <LoginIcon />
                         </div>
                         <input
-                            onChange={(e) => setUsername(e.target.value)}
-                            value={username}
+                            onChange={(e) => {
+                                setIsEmailValid(true);
+                                setEmail(e.target.value);
+                            }}
+                            value={email}
                             type="text"
                             className="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border-0 h-10 border-grey-light rounded rounded-l-none px-3 self-center relative sm:text-lg md:text-xl outline-none"
-                            placeholder="Username"
+                            placeholder="Email"
                             autoComplete="username"
                         />
                     </div>
+                    {!isEmailValid && <p className="text-red-500 text-sm italic -mt-3 mb-5">Field is required</p>}
                     <div className="flex flex-wrap items-stretch w-full relative h-15 bg-white items-center rounded">
                         <div className="flex -mr-px justify-center w-15 p-4">
                             <LockIcon />
                         </div>
                         <input
-                            onChange={(e) => setPassword(e.target.value)}
+                            onChange={(e) => {
+                                setIsPasswordValid(true);
+                                setPassword(e.target.value);
+                            }}
                             value={password}
                             type={`${viewPassword ? "text" : "password"}`}
                             className="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border-0 h-10 px-3 relative self-center sm:text-lg md:text-xl outline-none"
                             placeholder="Password"
                             autoComplete="current-password"
-                            required
                         />
                         <div
                             onClick={() => setViewPassword(!viewPassword)}
@@ -47,9 +88,10 @@ const LoginForm = () => {
                             <EyeIcon />
                         </div>
                     </div>
+                    {!isPasswordValid && <p className="text-red-500 text-sm italic mt-2">Field is required</p>}
                     <hr className="my-7" />
-                    <Link to="/" onClick={() => console.log(username + password)} type="submit">
-                        <p className="bg-neutral-600 text-center py-4 text-white hover:bg-neutral-500 active:bg-neutral-700 rounded tracking-wider sm:text-lg md:text-xl">
+                    <Link to="/" onClick={(e) => login(e)} type="submit">
+                        <p className="bg-neutral-600 text-center py-4 text-gray-50 hover:bg-neutral-700 hover:text-gray-100 active:bg-neutral-700 rounded tracking-wider sm:text-lg md:text-xl">
                             Login
                         </p>
                     </Link>
