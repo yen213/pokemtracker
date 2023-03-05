e3675f0e39e707c67c7bc1267a86a4ac4df21b33
Yen <hmaruf29@gmail.com>
2023-03-05 00:16:09 -0800

    create axios client and intercepter. TODO: add unauthorized and fail interceptor logic

---
+++ client/src/axios/axios.client.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+// Create axios client
+const axiosClient = axios.create({
+    baseURL: `http://localhost:3001`,
+    withCredentials: true,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+});
+
+// Interceptor for API calls
+axiosClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        let res = error.response;
+
+        if (res.status == 401) {
+            console.log(res);
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default axiosClient;
