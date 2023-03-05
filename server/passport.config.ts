98afd1e1cb415f8dc352b56318eb571a3d78744b
Yen <hmaruf29@gmail.com>
2023-03-04 19:51:25 -0800

    add passport config file

---
+++ server/passport.config.ts
@@ -0,0 +1,49 @@
+import * as AdminService from "./admin/admin.service";
+
+import { User } from "@prisma/client";
+import { PassportStatic } from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+
+// Find user matching the email and verify their password
+module.exports = function (passport: PassportStatic) {
+    passport.use(
+        new LocalStrategy(
+            {
+                usernameField: "email",
+                passwordField: "password",
+            },
+            async function verify(email: string, password: string, cb: Function) {
+                try {
+                    const user = await AdminService.findUserByEmail(email);
+
+                    if (!user) {
+                        return cb(null, false, { message: "Incorrect email" });
+                    } else if (user.password !== password) {
+                        return cb(null, false, { message: "Incorrect password" });
+                    }
+
+                    return cb(null, user);
+                } catch (err: any) {
+                    return cb(err);
+                }
+            }
+        )
+    );
+
+    // Serialize and store user session information
+    passport.serializeUser((user: User, cb) => {
+        cb(null, user.id);
+    });
+
+    // Deserialize user information from session storage
+    passport.deserializeUser(async (userId: number, cb) => {
+        try {
+            const user = await AdminService.findUserByID(userId);
+            const { id, email, first_name, last_name } = user;
+
+            return cb(null, { id, email, first_name, last_name });
+        } catch (err: any) {
+            return cb(err);
+        }
+    });
+};
